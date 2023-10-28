6dd5c3b2e12de2878aa3a7e14fac44072d886fa1
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-10-28 23:45:15 +0700

    Feat : Add router router or endpoint todo, and test server port 8000

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const pool = require('./config/connection.js');
+const port = 8000;
+const routerTodo = require('./models/todo.js')
+
+app.use(express.json());
+app.use('/', routerTodo)
+
+pool.connect((err, client) => {
+    if (err) {
+      console.error('Database Error', err);
+      return;
+    }
+    console.log('Connect to database succesfully');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running ${port}`);
+  });
+  
